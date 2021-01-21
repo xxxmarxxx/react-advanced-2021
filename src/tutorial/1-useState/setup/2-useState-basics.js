2e3fb455b8d0080fd27f09bd45dd817b905103c9
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 15:45:49 +0100

    lesson 38

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
+// use
+// component name must be uppercase
+// must be in the function/component body
+// cannot cal conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(use('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[0];
+  // console.log(value, handler);
+  const [text, setText] = useState('random title');
+
+  const handlerClick = () => {
+    if (text === 'random title'){
+      setText('hello world');
+    } else {
+      setText('random title');
+    }
+  };
+  return (
+    <>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handlerClick}>
+      change title
+    </button>
+    </>
+  )
 };
 
 export default UseStateBasics;
