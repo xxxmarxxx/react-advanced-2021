f92b505bdade0bd651689d8ff5ec9ef201d53bab
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 15:34:43 +0100

    lesson 37

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  let title = 'random title'
-  const handlerClick = ()=> {
-    title = 'hello people'
+  let title = "random title";
+  const handlerClick = () => {
+    title = "hello people";
     console.log(title);
-  }
+  };
   return (
-
-  <>
-  <h2>{title}</h2>
-  <button type="button" className='btn' onClick={handlerClick}>
-    change title
-  </button>
-  </>
-
-
-  )
+    <>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handlerClick}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
