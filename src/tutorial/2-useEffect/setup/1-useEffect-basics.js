f4d0c454fc1de373fd7073a6c7da5faae3a144b6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 17:03:45 +0100

    lesson 46 useEffect - basic

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
+// domyślnie uruchamiana po każdym renderowaniu
 // cleanup function
+// funkcja czyszcząca
 // second parameter
+// drugi parametr
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("call useEffect");
+    document.title = `New Massages(${value})`;
+  });
+  console.log("render component");
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
