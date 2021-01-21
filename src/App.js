bc0b8a9b46a06e4d0da6a38a2c68182eb3eeb6fa
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 15:10:49 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Setup from './tutorial/1-useState/setup/1-error-example';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <Setup  />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ErrorExample = () => {
+  let title = 'random title'
+  const handlerClick = ()=> {
+    title = 'hello people'
+    console.log(title);
+  }
+  return (
+
+  <>
+  <h2>{title}</h2>
+  <button type="button" className='btn' onClick={handlerClick}>
+    change title
+  </button>
+  </>
+
+
+  )
+};
+
+export default ErrorExample;
