625fe76ea527e550fb4d03eab81f782bb5740747
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-21 15:53:03 +0100

    lesson 39 ogólne zasady dotyczące hooks

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // use
 // component name must be uppercase
 // must be in the function/component body
@@ -9,23 +9,24 @@ const UseStateBasics = () => {
   // const value = useState(1)[0];
   // const handler = useState(1)[0];
   // console.log(value, handler);
-  const [text, setText] = useState('random title');
+  
+  const [text, setText] = useState("random title");
 
   const handlerClick = () => {
-    if (text === 'random title'){
-      setText('hello world');
+    if (text === "random title") {
+      setText("hello world");
     } else {
-      setText('random title');
+      setText("random title");
     }
   };
   return (
     <>
-    <h1>{text}</h1>
-    <button className="btn" onClick={handlerClick}>
-      change title
-    </button>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handlerClick}>
+        change title
+      </button>
     </>
-  )
+  );
 };
 
 export default UseStateBasics;
