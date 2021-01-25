64f8d301d9b4e6436580a0bc4c30924a9299163a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-25 13:13:54 +0100

    lesson 54 ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
@@ -6,18 +6,30 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  const [text,setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
-  return <>
-  {/* <h1>{firstValue}</h1>
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
   <h1>value: {secondValue}</h1> */}
-  {/* {if(){console.log('hello')}} */}
-  <h1>{text || 'john doe'}</h1>
-  {text && <h1>hello world</h1>}
-  {!text && <h1>hello world</h1>}
-  </>;
+      {/* {if(){console.log('hello')}} */}
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
