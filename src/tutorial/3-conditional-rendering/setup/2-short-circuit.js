7d9d6ec3962c12e981fb91ae144fd9089483ccb6
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-25 12:50:02 +0100

    lesson 53 short-circuit evalution

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,7 +6,18 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [text,setText] = useState('');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+
+  return <>
+  {/* <h1>{firstValue}</h1>
+  <h1>value: {secondValue}</h1> */}
+  {/* {if(){console.log('hello')}} */}
+  <h1>{text || 'john doe'}</h1>
+  {text && <h1>hello world</h1>}
+  {!text && <h1>hello world</h1>}
+  </>;
 };
 
 export default ShortCircuit;
