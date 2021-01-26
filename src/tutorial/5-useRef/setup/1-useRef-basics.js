09e25f644b1909c65512b6834d8c9041bc58f2c7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-26 14:35:49 +0100

    first commit lesson 62

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
+// zachowuje wartość
 // DOES NOT trigger re-render
+// nie powoduje ponownego renderowania
 // target DOM nodes/elements
+// docelowe węzły/elementy DOM
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  return (
+    <h2>
+      useRef
+    </h2>
+  );
 };
 
 export default UseRefBasics;
