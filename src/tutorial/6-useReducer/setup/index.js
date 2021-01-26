37b181da32c8deb5188a5195ee51abbd5998c08e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-26 17:17:32 +0100

    update

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,44 +3,41 @@ import Modal from "./Modal";
 import { data } from "../../../data";
 // lesson 64/65 useReducer - useState Setup
 // reducer function
-const reducer = (state,action)=>{
-
-}
-const defaultState={
-  people: data,
-  isModalOpen:true,
-  modalContent:'hello world'
-}
+const reducer = (state, action) => {};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "hello world",
+};
 const Index = () => {
   const [name, setName] = useState("");
-  const [state,dispatch] = useReducer(reducer,defaultState)
+  const [state, dispatch] = useReducer(reducer, defaultState);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      
     } else {
-      
     }
   };
 
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-
           />
         </div>
         <button type="submit">add</button>
       </form>
-      {state.people.map((person)=>{
-        return <div key={personalbar.id}>
-          <h4>{person.name}</h4>
-        </div>
+      {state.people.map((person) => {
+        return (
+          <div key={personalbar.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
       })}
     </>
   );
