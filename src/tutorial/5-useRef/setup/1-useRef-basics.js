61d20b61b65d6456022170ed567b831c57b08d17
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-26 15:42:28 +0100

    lesson 62 useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-
+// lesson 62
 // preserves value
 // zachowuje wartość
 // DOES NOT trigger re-render
@@ -8,10 +8,27 @@ import React, { useEffect, useRef } from "react";
 // docelowe węzły/elementy DOM
 
 const UseRefBasics = () => {
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
   return (
-    <h2>
-      useRef
-    </h2>
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>hello world</div>
+    </>
   );
 };
 
