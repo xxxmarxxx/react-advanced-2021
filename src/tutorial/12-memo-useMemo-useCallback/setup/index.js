4d0ee30c1275ac9a394fb059438b35aa316a438e
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-27 18:19:32 +0100

    lesson 82 use Callback

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,54 +1,67 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
-// lesson 80/81 React.memo
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
+// lesson 80/81/82 useCallback
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = "https://course-api.com/javascript-store-products";
 
 // every time props or state changes, component re-renders
 
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
 
   return (
     <>
       <h1>Count : {count}</h1>
-      <button className='btn' onClick={() => setCount(count + 1)}>
+      <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>cart : {cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
-  )
-}
+  );
+};
 
-const BigList = React.memo(({ products }) => {
-  useEffect(()=>{
-    console.log('big list called');
-  })
+const BigList = React.memo(({ products, addToCart }) => {
+  useEffect(() => {
+    console.log("big list called");
+  });
   return (
-    <section className='products'>
+    <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
-  )
-})
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  useEffect(()=>{
-    console.count('single item called');
-  })
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
+const SingleProduct = ({ fields, addToCart }) => {
+  useEffect(() => {
+    console.count("single item called");
+  });
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
 
   return (
-    <article className='product'>
+    <article className="product">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
