b1d6e1f4bd86225a2e559cd17d586d47a8de3bf2
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-27 14:31:53 +0100

    lesson 71 Custom Hooks - useFetch

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+// lesson 71 Custom Hooks - useFetch
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
-export const useFetch = () => {};
+  const getProducts = async () => {
+    const response = await fetch(url);
+    const products = await response.json();
+    setProducts(products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [url]);
+  return { loading, products };
+};
