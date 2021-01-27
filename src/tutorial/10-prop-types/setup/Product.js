df70a473aeb9c8ab7492ea01e0a1c9dde9a2ee8a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-27 15:16:20 +0100

    lesson 73 PropTypes - Images

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = ({image,name,price}) => {
-  console.log(image,name,price);
-  return <article className='product'>
-    <h4>single product</h4>
-    {/* <img src={image.url} alt={name}/>
+const Product = ({ image, name, price }) => {
+  console.log(image, name, price);
+  return (
+    <article className="product">
+      
+      <img src={image.url} alt={name}/>
     <h4>{name}</h4>
-    <p>${price}</p> */}
-  </article>;
+    <p>${price}</p>
+    </article>
+  );
+};
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+Product.defaultProps = {
+  name: "default",
+  price: 3.99,
+  image: defaultImage,
 };
 
 export default Product;
--- src/tutorial/10-prop-types/setup/index.js
+++ src/tutorial/10-prop-types/setup/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import Product from './Product'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
-// lesson 72 PropTypes - Setup
+import React from "react";
+import Product from "./Product";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
+// import defaultImage from '../../../assets/default-image.jpeg';
+
+// lesson 72/73 PropTypes - Images
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-prop-types-example'
+const url = "https://course-api.com/react-prop-types-example";
 
 const Index = () => {
-  const { products } = useFetch(url)
+  const { products } = useFetch(url);
   return (
     <div>
       <h2>products</h2>
-      <section className='products'>
+      {/* <img src={defaultImage} /> */}
+      <section className="products">
         {products.map((product) => {
-          return <Product key={product.id} {...product} />
+          return <Product key={product.id} {...product} />;
         })}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
